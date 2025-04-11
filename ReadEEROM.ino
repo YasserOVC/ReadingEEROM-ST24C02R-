d80253adba5497f2d94de332b18693cb4f3c32b9
YasserOVC <engyasser130@yahoo.com>
2025-04-11 22:26:32 +0200

    reading and another code for address

--- ReadEEROM.ino
+++ ReadEEROM.ino
@@ -1,59 +1,38 @@
 #include <Wire.h>
 
-// Define the EEPROM address.  The ST24C02R typically has an address of 0x50.
-#define EEPROM_ADDRESS 0x50
-
-// Define the size of the EEPROM.  The ST24C02R is 256 bytes (2Kbit).
-#define EEPROM_SIZE 256
-
 void setup() {
   Serial.begin(9600);
   Wire.begin();
 
-  Serial.println("Reading ST24C02R EEPROM...");
-  delay(500); // Increased delay
+  Serial.println("I2C Scanner");
 }
 
 void loop() {
-  // Read and print the entire EEPROM contents.
-  for (int address = 0; address < EEPROM_SIZE; address++) {
-    byte data = readEEPROMByte(address);
-    Serial.print("Address 0x");
-    if (address < 16) {
-      Serial.print("0");
+  byte error, address;
+  int nDevices;
+
+  nDevices = 0;
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
+      nDevices++;
+    } else if (error == 4) {
+      Serial.print("Unknown error at address 0x");
+      if (address < 16)
+        Serial.print("0");
+      Serial.println(address, HEX);
     }
-    Serial.print(address, HEX);
-    Serial.print(": 0x");
-    if (data < 16) {
-      Serial.print("0");
-    }
-    Serial.println(data, HEX);
-    delay(10); // Increased delay
   }
-  Serial.println("Finished reading EEPROM.\n");
-  while (1); // Stop after reading once
-}
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\\n");
+  else
+    Serial.println("done\\n");
 
-// Function to read a single byte from the EEPROM at a given address.
-byte readEEPROMByte(int address) {
-  byte data = 0;
-  int error;
-
-  Wire.beginTransmission(EEPROM_ADDRESS);
-  Wire.write(address);         // Set the address to read from.
-  error = Wire.endTransmission();
-  if (error != 0) {
-    Serial.print("Error setting address: ");
-    Serial.println(error);
-    return 0xFF; // Return a default error value
-  }
-
-  Wire.requestFrom(EEPROM_ADDRESS, 1); // Request one byte.
-  if (Wire.available()) {
-    data = Wire.read();
-  } else {
-    Serial.println("No data available");
-    return 0xFF;
-  }
-  return data;
+  delay(5000); // Wait 5 seconds for next scan
 }
